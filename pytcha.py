5b71529d7f17f555427353aece321c9127178abd
Jeremy Stevens <jeremiahstevens@gmail.com>
2022-01-13 22:02:01 -0800

    a few changes

--- pytcha.py
+++ pytcha.py
@@ -3,16 +3,15 @@ pytcha is a tool for generating different styles of captcha images for Flask Pro
 or other python web applications
 """
 import string
+import random
 
 from PIL import Image, ImageDraw, ImageFont
-import random
 
 
 class Generate:
 
     def __init__(self):
-        get_number = None
-        size = None
+        pass
 
     " get a captcha with just number "
 
@@ -20,8 +19,8 @@ class Generate:
     def captcha_numbers(size):
         get_number = str(random.randrange(1000, 99999, size))
         img = Image.new('RGB', (90, 30), color=(73, 109, 137))
-        d = ImageDraw.Draw(img)
-        d.text((10, 10), get_number, fill=(255, 255, 0))
+        d_img = ImageDraw.Draw(img)
+        d_img.text((10, 10), get_number, fill=(255, 255, 0))
         img.save('captcha.png')
         print(get_number)
         return get_number
@@ -32,9 +31,9 @@ class Generate:
     def captcha_combo(chars=string.ascii_uppercase + string.digits):
         result_str = ''.join((random.choice(chars) for i in range(6)))
         img = Image.new('RGB', (95, 40), color=(73, 109, 137))
-        d = ImageDraw.Draw(img)
+        d_img = ImageDraw.Draw(img)
         fnt = ImageFont.truetype("FreeMono.ttf", 16)
-        d.text((10, 10), result_str, font=fnt, fill=(255, 255, 0))
+        d_img.text((10, 10), result_str, font=fnt, fill=(255, 255, 0))
         img.save('captcha.png')
         print(str(result_str))
 
